8bfb692fc1737364b2739899f359ad0bad7e4592
DM-98 <dannymoller@live.dk>
2024-10-13 16:06:49 +0200

    Simplify project by removing configs and dependencies
    Significantly cleaned up `Faluf.Trading.AppHost.csproj` by removing all property groups, item groups, and project references. Simplified `Program.cs` by eliminating `IDistributedApplicationBuilder` setup. Removed `launchSettings.json`, `appsettings.Development.json`, and `appsettings.json` files. Reformatted `@code` block in `CultureSelector.razor` and adjusted `uri` variable assignment. Deleted `Extensions.cs` and `Faluf.Trading.ServiceDefaults.csproj`, removing extensions for .NET Aspire services and related package references.

--- Faluf.Trading.AppHost/Program.cs
+++
@@ -1,5 +0,0 @@
-IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.Faluf_Trading_Blazor>("blazor-rendermode-auto").WithExternalHttpEndpoints();
-
-builder.Build().Run();
